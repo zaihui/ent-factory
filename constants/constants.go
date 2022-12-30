4af2bfd1c22636b96b7db5a4b8541c7ec3d29cee
Xinyan Lu <lvxinyan1992@gmail.com>
2022-12-30 17:03:44 +0800

    EF-9(refactor): extract constants

--- constants/constants.go
+++ constants/constants.go
@@ -1,11 +1,15 @@
 package constants
 
 var (
-	FakeDataFunc       = "faker.FakeData" // faker library faker data func
-	SuiteCaseType      = "factories.TestSuite"
-	SuiteCaseVariable  = "s"       // instance of suite case
-	SuiteNoErrorFunc   = "NoError" // error check func for suite case
-	FactoryNewFuncName = "New"
-	SkipStructFields   = map[string]struct{}{"Edges": {}}
-	Perm               = 0o777
+	FakeDataFunc         = "faker.FakeData" // faker library faker data func
+	SuiteCaseType        = "factories.TestSuite"
+	SuiteCaseVariable    = "s"       // instance of suite case
+	SuiteNoErrorFunc     = "NoError" // error check func for suite case
+	FactoryNewFuncName   = "New"
+	SkipStructFields     = map[string]struct{}{"Edges": {}}
+	Perm                 = 0o777
+	DefaultAppPath       = "service/app"
+	DefaultFactoryPath   = "factories"
+	DefaultEntClientName = "EntClient"
+	IngoreFolderNames    = []string{"enttest", "hook", "migrate", "partnerregister", "predicate", "runtime"}
 )
